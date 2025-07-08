df0bf6a7d162cc45577af419b644c94791075923
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2025-07-08 11:26:00 +0300

    Add more indicator samples.

---
+++ Indicators/AccountType Sample/AccountType Sample/Engine.cs
@@ -0,0 +1,60 @@
+using System;
+using cAlgo.API;
+using Python.Runtime;
+
+namespace cAlgo.Indicators;
+
+[Indicator(AccessRights = AccessRights.None, IsOverlay = true)]
+public class AccountTypeSample : Indicator
+{
+    private IndicatorBridge _indicator;
+    private const string MainPythonFile = "AccountType Sample_main.py";
+
+    protected override void Initialize()
+    {
+        EngineHelper.Initialize(this);
+
+        using (Py.GIL())
+        {
+            var code = EmbeddedResourceProvider.ReadText(MainPythonFile);
+            var className = EngineHelper.GetClassName(code);
+
+            using (var scope = Py.CreateScope())
+            {
+                scope.Set("currentAssembly", System.Reflection.Assembly.GetExecutingAssembly());
+
+                try
+                {
+                    scope.Exec(code);
+                    if (!scope.Contains(className))
+                    {
+                        Print($"Error: Python class '{className}' not found in the module!");
+                        throw new InvalidOperationException($"Python class '{className}' not found in the module");
+                    }
+
+                    dynamic pythonClass = scope.Get(className);
+                    _indicator = new IndicatorBridge(pythonClass());
+
+                    _indicator.Initialize();
+                }
+                catch (Exception ex)
+                {
+                    Print($"Error initializing Python: {ex.Message}\nStack trace: {ex.StackTrace}");
+                    throw;
+                }
+            }
+        }
+    }
+
+    public override void Calculate(int index)
+    {
+        using (Py.GIL())
+            _indicator.Calculate(index);
+    }
+
+    protected override void OnDestroy()
+    {
+        using (Py.GIL())
+            _indicator.OnDestroy();
+    }
+}
