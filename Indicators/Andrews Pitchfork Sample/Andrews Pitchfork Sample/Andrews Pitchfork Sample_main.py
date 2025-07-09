1f32752a029ed97345f9a2a92f4c7917e7a5329a
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2025-07-09 15:10:49 +0300

    XT-17608 Create cBots, Indicators and Plugins Samples in Python. Refactor indicator Andrews Pitchfork Sample.

--- Indicators/Andrews Pitchfork Sample/Andrews Pitchfork Sample/Andrews Pitchfork Sample_main.py
+++ Indicators/Andrews Pitchfork Sample/Andrews Pitchfork Sample/Andrews Pitchfork Sample_main.py
@@ -9,9 +9,9 @@ class AndrewsPitchforkSample():
         barIndex2 = api.Chart.FirstVisibleBarIndex + ((api.Chart.LastVisibleBarIndex - api.Chart.FirstVisibleBarIndex) // 5)
         barIndex3 = api.Chart.FirstVisibleBarIndex + ((api.Chart.LastVisibleBarIndex - api.Chart.FirstVisibleBarIndex) // 2)
 
-        y1 = api.Bars.ClosePrices.Last(api.Bars.ClosePrices.Count - barIndex1)
-        y2 = api.Bars.ClosePrices.Last(api.Bars.ClosePrices.Count - barIndex2)
-        y3 = api.Bars.ClosePrices.Last(api.Bars.ClosePrices.Count - barIndex3)
+        y1 = api.Bars.ClosePrices[barIndex1]
+        y2 = api.Bars.ClosePrices[barIndex2]
+        y3 = api.Bars.ClosePrices[barIndex3]
        
         andrewsPitchfork = api.Chart.DrawAndrewsPitchfork("AndrewsPitchfork", barIndex1, y1, barIndex2, y2, barIndex3, y3, Color.Red)
         andrewsPitchfork.IsInteractive = True
