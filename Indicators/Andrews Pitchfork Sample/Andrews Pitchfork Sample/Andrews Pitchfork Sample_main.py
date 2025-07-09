0ab25a45d3346cc49be1ee9bcd4210c5153ed50e
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2025-07-09 12:08:43 +0300

    Add more indicator samples.

---
+++ Indicators/Andrews Pitchfork Sample/Andrews Pitchfork Sample/Andrews Pitchfork Sample_main.py
@@ -0,0 +1,20 @@
+import clr
+clr.AddReference("cAlgo.API")
+
+from cAlgo.API import *
+
+class AndrewsPitchforkSample():
+    def initialize(self):
+        barIndex1 = api.Chart.FirstVisibleBarIndex
+        barIndex2 = api.Chart.FirstVisibleBarIndex + ((api.Chart.LastVisibleBarIndex - api.Chart.FirstVisibleBarIndex) // 5)
+        barIndex3 = api.Chart.FirstVisibleBarIndex + ((api.Chart.LastVisibleBarIndex - api.Chart.FirstVisibleBarIndex) // 2)
+
+        y1 = api.Bars.ClosePrices.Last(api.Bars.ClosePrices.Count - barIndex1)
+        y2 = api.Bars.ClosePrices.Last(api.Bars.ClosePrices.Count - barIndex2)
+        y3 = api.Bars.ClosePrices.Last(api.Bars.ClosePrices.Count - barIndex3)
+       
+        andrewsPitchfork = api.Chart.DrawAndrewsPitchfork("AndrewsPitchfork", barIndex1, y1, barIndex2, y2, barIndex3, y3, Color.Red)
+        andrewsPitchfork.IsInteractive = True
+        
+    def calculate(self, index):
+        pass
---
+++ Indicators/Application Sample/Application Sample/Application Sample_main.py
@@ -0,0 +1,49 @@
+import clr
+clr.AddReference("cAlgo.API")
+
+from cAlgo.API import *
+
+def getTextBlock(text):
+    textBlock = TextBlock()
+    textBlock.Text = text
+    textBlock.Margin = Thickness(5)
+    return textBlock
+
+class ApplicationSample():
+    def initialize(self):
+        api.Application.ColorThemeChanged += lambda obj: self.applicationColorThemeChanged(obj)
+        api.Application.UserTimeOffsetChanged += lambda obj: self.applicationUserTimeOffsetChanged(obj)
+        self.drawApplicationInfo()
+        
+    def calculate(self, index):
+        pass
+
+    def drawApplicationInfo(self):
+        grid = Grid(3, 2)
+        grid.BackgroundColor = Color.Goldenrod
+        grid.HorizontalAlignment = api.HorizontalAlignment
+        grid.VerticalAlignment = api.VerticalAlignment
+
+        grid.AddChild(getTextBlock("Version"), 0, 0)
+        grid.AddChild(getTextBlock(str(api.Application.Version)), 0, 1)
+        grid.AddChild(getTextBlock("Theme"), 1, 0)
+
+        grid.AddChild(getTextBlock("Theme"), 1, 0)
+
+        self.themeTextBlock = getTextBlock(str(api.Application.ColorTheme))
+
+        grid.AddChild(self.themeTextBlock, 1, 1)
+
+        grid.AddChild(getTextBlock("User Time Offset"), 2, 0)
+
+        self.userTimeOffsetTextBlock = getTextBlock(str(api.Application.UserTimeOffset))
+
+        grid.AddChild(self.userTimeOffsetTextBlock, 2, 1)
+
+        api.Chart.AddControl(grid)
+
+    def applicationUserTimeOffsetChanged(self, obj):
+        self.userTimeOffsetTextBlock.Text = str(obj.UserTimeOffset)
+
+    def applicationColorThemeChanged(self, obj):
+        self.themeTextBlock.Text = str(obj.ColorTheme)
