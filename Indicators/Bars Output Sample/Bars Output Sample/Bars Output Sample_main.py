775dd1c266a8a78032cdaeba55f1e7221e366e85
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2025-08-20 10:20:00 +0300

    XT-17769 Add “Bar Outputs” sample to cTrader Algo Samples repo. Add new indicator sample for Bars output.

---
+++ Indicators/Bars Output Sample/Bars Output Sample/Bars Output Sample_main.py
@@ -0,0 +1,26 @@
+import clr
+clr.AddReference("cAlgo.API")
+
+from cAlgo.API import *
+from System import Int64
+
+class BarsOutputSample():
+    def initialize(self):
+        self.openEma = api.Indicators.ExponentialMovingAverage(api.Bars.OpenPrices, api.EmaPeriods)
+        self.highEma = api.Indicators.ExponentialMovingAverage(api.Bars.HighPrices, api.EmaPeriods)
+        self.lowEma = api.Indicators.ExponentialMovingAverage(api.Bars.LowPrices, api.EmaPeriods)
+        self.closeEma = api.Indicators.ExponentialMovingAverage(api.Bars.ClosePrices, api.EmaPeriods)
+
+        # Hide main chart bars
+        api.Chart.DisplaySettings.Bars = False
+
+        # You can change output colors and other appearance properties via code
+        # api.Result.Output.BullFillColor = Color.Blue
+        # api.Result.Output.BullOutlineColor = Color.Blue
+        # api.Result.Output.BearFillColor = Color.Gold
+        # api.Result.Output.BearOutlineColor = Color.Gold
+        # api.Result.Output.ChartType = ChartType.Bars
+        # api.Result.Output.TickVolumeColor = Color.Magenta
+        
+    def calculate(self, index):
+        api.Result[index] = OhlcvBar(self.openEma.Result[index], self.highEma.Result[index], self.lowEma.Result[index], self.closeEma.Result[index], int(api.Bars.TickVolumes[index]))
